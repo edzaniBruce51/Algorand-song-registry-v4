c4b92e414977258d431fa34465540e3a0c7b299d
edzaniBruce51 <mutemulaeb51@gmail.com>
2025-08-20 10:34:51 +0200

    updated the debug to true to spot the real error

--- app.py
+++ app.py
@@ -130,4 +130,4 @@ def blockchain_webhook():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
